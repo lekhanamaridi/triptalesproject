29ba3a6c4f96719129f81e13298060f52db053a3
anushaanadiger <anushaanadiger@gmail.com>
2023-11-15 13:46:06 +0530

    tc

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,30 @@
-import main from './images/main/main photo.jpg';
+import main from './images/pilgrimage/kedarnath 1.jpg';
 import logofile from './images/main/logofile.png';
+import main1 from './images/treking/kedarnath-trek 2.jpg';
+import log from './images/main/log.jpg';
+import './Header.css'
 
 function Header()
 {
     return(
-        <div>
-            <div>
-                <img src={logofile}></img>
-                <h1>Trip Tales</h1>
+        <div className='header'>
+            <div className='heading'>
+                <img className='lf' src={logofile}></img>
+                <h1 className='text'><i>Trip Tales</i></h1>
+                
             </div>
-            <div>
+            <div className='quote'>
                 <img src={main} className='main'></img>
+                <h2 className='text1'>"Take only memories,leave only footprints.."</h2>
             </div>
+            <div className='quote'>
+                <h2 className='text2'>"Always say yes to new adventures.." </h2>
+                <img className='main' src={main1}></img>
+            </div>
+            <div >
+                <button className='button' type='button'><h2><i> -- Plan Your Trip --</i></h2></button>
+            </div>
+            
         </div>
     );
 }
